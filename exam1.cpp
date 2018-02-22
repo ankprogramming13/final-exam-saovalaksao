70d50445dd262d7c70e6ce916c34b8147f6368eb
Unknown <saovalaksao@gmail.com>
2018-02-22 12:32:55 +0700

    1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-
+using namespace std ;
 int main() {
+    cout <<"welcome to CLI Calculator V1.00"<< endl;
+    int a,b;
+    cin >> a;
+    cin >> b;
+    int cmd;
 
-        return 0;
-}
+    cout <<"+"<< endl;
+    cout <<"-"<< endl;
+    cout <<"*"<< endl;
+    cout <<"/"<< endl;
+    cout <<"quit"<< endl;
+
+   do {
+    cin >>cmd;
+    if (a+b){
+
+    }
+   }while (cmd == 1);
+   if (a-b){
+
+   }while (cmd == 2);
+   if (a*b){
+
+   }while (cmd == 3);
+    if (a/b){
+
+    }while (cmd == 4);
+
+    }
+    cout << "Resit=...";
+
+    return 0;
+    }
+
